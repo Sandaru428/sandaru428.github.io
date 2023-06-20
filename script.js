6ad348ff0b78a1862b23a7c065d7da764f6a006b
Sandaruwan S.W.C <130985816+Sandaru428@users.noreply.github.com>
2023-06-20 16:01:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/*============= typed js =============*/
+
+const typed = new Typed(".multiple-text", {
+    strings: ['Undergraduate', 'Developer', 'Freelancer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
